750b950245091d15bab5ac4f9c66219a22e8897e
Batman <bgmyhan@gmail.com>
2020-06-04 16:07:51 -0700

    created basic template for readMe that can utilize two user options. Can't seem to figure out how to make more than two.

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+const fs = require('fs') 
 const inquirer = require('inquirer');
 
 inquirer
 .prompt([
   {
-    name: 'newquestions',
-    message: 'what is your name?',
+    name: "websiteName",
+    message: 'On what website will this readme be displayed?',
+    default: 'www.yourwebsitehere.com'
   },
   {
-      name: 'moreTime',
-      message: 'OneMoreTime'
+      type: "input", 
+      name: 'companyName',
+      message: 'What company or person(s) is this readme for?',
+      default: 'Joe Schmoe LLC'
   },
 ])
 .then(answers => {
-console.log(answers.newquestions)
+
+// Data which will write in a file. 
+let data = "A Read Me by " + answers.websiteName + "." + " For " + answers.companyName + "." + " Prerequisites: What things you need to install the software and how to install them." + 
+
+" Installing: A step by step series of examples that tell you how to get a development env running. Say what the step will be. Give an example. Repeat until finished. End with an example of getting some data out of the system or using it for a little demo" +
+
+" Running the tests! Explain how to run the automated tests for this system. Break down into end to end tests. Explain what these tests test and why. Give an example. And coding style tests. Explain what these tests test and why Give an example" +
+
+" Deployment. Add additional notes about how to deploy this on a live system";
+
+
+// Write data in 'Output.txt' . 
+fs.writeFile('readme.md', data, (err) => {
+      
+   
+    if (err) throw err; 
+}) 
+
 })
 .catch(error => {
     if(error.isTtyError) {
@@ -22,5 +43,4 @@ console.log(answers.newquestions)
     //   console.log(isTtyError)
     }
   });
-
 
